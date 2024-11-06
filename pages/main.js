c9cfda2396e4ba97e0ba806fc5b5483301550f1e
shivank04 <shivankar.rockit@gmail.com>
2024-11-06 17:47:55 +0530

    fix

--- pages/main.js
+++ pages/main.js
@@ -49,6 +49,19 @@ export default function MainPage() {
     const [followUpMessage, setFollowUpMessage] = useState('');
     const [error, setError] = useState('');
 
+    axios.interceptors.request.use(
+        (config) => {
+            const token = localStorage.getItem('authToken');
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (!token) {
